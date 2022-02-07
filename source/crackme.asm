18ecc04c0cd748f5351ce83e29db5fcf43fda33d
nn653 <41921371+nn653@users.noreply.github.com>
2022-02-07 21:52:03 +0200

    update

--- source/crackme.asm
+++ source/crackme.asm
@@ -1,107 +1,58 @@
-; YO BRO ARE YOU SO WEAKLY?
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-;
-
-
 .model small
 .stack 100h
 .data
 	titlemsg db "Enter serial number: $"
 	correctmsg db "The serial number is correct. You have successfully logged in!$"
 	incorrectmsg db "Invalid serial number.$"
-	
-	correctserialnum db 0C9h, 0CEh, 0DAh, 0C1h, 0C3h, 0C9h
+	correctserialnum db 0C9h, 0CFh, 0D9h, 0C7h, 0D9h, 0D6h
 .data?
 	inputserialnum db 80h, ?, 80h dup(?)
 .code
 	START:
 		mov BX, @data
-		mov DS, BX
-		
+		mov DS, BX		
 		mov AH, 9h
 		mov DX, offset titlemsg
-		int 21h
-		
+		int 21h		
 		mov AH, 0Ah
 		mov DX, offset inputserialnum
 		int 21h
-		
 		mov BX, DX
 		mov AH, 0h
 		mov AL, byte ptr [BX + 1]
 		add BX, AX
 		mov word ptr [BX + 3], 240Ah
-
 		mov AH, 02h
 		mov DL, 0Dh
 		int 21h
 		mov DL, 0Ah
 		int 21h
-		
-
 		mov SI, offset correctserialnum
-		mov DI, offset inputserialnum + 2
-		
+		mov DI, offset inputserialnum + 2		
 		mov AL, byte ptr [DI]
 		mov BH, 0h
-		mov BL, byte ptr [DI - 1]	
+		mov BL, byte ptr [DI - 1]
 		add AL, byte ptr [DI + BX - 1]
 		add AL, BL
-		
-		mov BX, 0h
-		
+		mov BX, 0h	
 		loop1:
-			cmp BX, 6h
-			jge correctserial
-			
-			mov CL, byte ptr [DI + BX]
-			xor CL, AL
-			cmp CL, byte ptr [SI + BX]
-			jne invalidserial
-			inc BX
-			jmp loop1
-			
+		cmp BX, 6h
+		jge correctserial
+		mov CL, byte ptr [DI + BX]
+		xor CL, AL
+		cmp CL, byte ptr [SI + BX]
+		jne invalidserial
+		inc BX
+		add AL, BL
+		jmp loop1	
 		invalidserial:
-			mov DX, offset incorrectmsg
-			jmp endofprogram
-		
+		mov DX, offset incorrectmsg
+		jmp endofprogram	
 		correctserial:
-			mov DX, offset correctmsg
-			
+		mov DX, offset correctmsg	
 		endofprogram:
-			mov AH, 9h
-			int 21h
-		
+		mov AH, 9h
+		int 21h
 		mov AX, 4C00h
 		int 21h
 end START
